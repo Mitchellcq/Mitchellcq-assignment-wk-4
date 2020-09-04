d389cbaf082c2f0914c3332bc8d663ce2ba3da13
Chee Quee <mitchellcq@gmail.com>
2020-09-04 14:11:51 +1000

    added several javascript functions

--- scriptMain.js
+++ scriptMain.js
@@ -1,27 +1,74 @@
 var Qcard = document.getElementById("Question-card");
-var Qnumber = document.getElementById("Question-Number");
 var Qtext = document.getElementById("Question-text");
 var Qanswers = document.getElementById("Question-answers");
+var Qindex = document.getElementById("Question-index");
+var Qcount = 5;
 
+//question objects
 
+var questionOne = {question:"",
+                    answers:"","","","",
+                };
 
+var questionTwo = {question:"",
+                    answers:"","","","",
+                };
 
+var questionThree = {question:"",
+                    answers:"","","","",
+                };
 
+var questionFour = {question:"",
+                    answers:"","","","",
+                };
 
+var questionFive = {question:"",
+                    answers:"","","","",
+                };
+
+var questionSix = {question:"",
+                    answers:"","","","",
+                };
 
 
 init();
 
 function init() {
-   Qcard.style.visibility = "hidden";
+   Qcard.style.display = "none";
 }
 
+function ask() {
+    for (i=0; i < Qcount; i++) {
+    Qindex.value = [i];
+    
+    if (i=0){
+        Qtext.textContent = questionOne.question;
+        Qanswers.textContent = questionOne.answers;
+    } else if (i=1){
+        Qtext.textContent = questionOne.question;
+        Qanswers.textContent = questionOne.answers;
+    } else if (i=2){
+        Qtext.textContent = questionOne.question;
+        Qanswers.textContent = questionOne.answers;
+    } else if (i=3){
+        Qtext.textContent = questionOne.question;
+        Qanswers.textContent = questionOne.answers;
+    } else if (i=4){
+        Qtext.textContent = questionOne.question;
+        Qanswers.textContent = questionOne.answers;
+    } else (i=5){
+        Qtext.textContent = questionOne.question;
+        Qanswers.textContent = questionOne.answers;
+    }
 
+
+}
 
 function showQuestions(event) {
     var x = document.getElementById("Question-card");
-    if (x.style.visibility === "hidden") {
-      x.style.visibility = "visible";
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      ask();
     }
 }
 
